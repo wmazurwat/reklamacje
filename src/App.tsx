059b5bb62db73b92abb8d61a51156d876aa8f39b
wmazurwat <wojciech.mazur.lo@gmail.com>
2022-09-09 18:28:49 +0200

    update 09/09/2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <h1>Welcome to React Router!</h1>
+       <nav>
+        <Link to="/login">login</Link>
+        <Link to="/">Home</Link>
 
-      </header>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
